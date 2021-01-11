c9f7ef7c8f621ca32d7a7da284c3a1e5d352fc04
changangus <63685366+changangus@users.noreply.github.com>
2021-01-11 12:31:03 -0500

    styled collection item and collection preview, and passed all necessary props for rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 // dev dependent components
 import { Route, Switch } from 'react-router-dom';
 // page components
--- src/components/collection-preview/CollectionPreview.component.jsx
+++ src/components/collection-preview/CollectionPreview.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './CollectionPreview.styles.scss';
+import CollectionItem from '../collection-item/CollectionItem.component';
 
 const CollectionPreview = ({title, items}) => {
   return (
     <div className='collection-preview'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <div className='preview'>
-        {items.filter((item, idx) => idx < 4).map(item => (
-          <div id={item.id}>{item.name}</div>
+        {items.filter((item, idx) => idx < 4).map(({id, ...props}) => (
+          <CollectionItem key={id} {...props}/>
         ))}
       </div>
     </div>
