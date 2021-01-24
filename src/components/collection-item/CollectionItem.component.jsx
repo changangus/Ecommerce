b3ee427c51b7cd111d3c5330f4d0d23146f9882b
changangus <63685366+changangus@users.noreply.github.com>
2021-01-24 12:44:37 -0500

    addItem action defined in cart reducer and passed to collection item component

--- src/components/collection-item/CollectionItem.component.jsx
+++ src/components/collection-item/CollectionItem.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import CustomButton from '../custom-button/CustomButton.component';
 import './CollectionItem.styles.scss';
+// Redux:
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
-
-const CollectionItem = ({id, name, price, imageUrl}) => {
+const CollectionItem = ({item, addItem}) => {
+  const { imageUrl, name, price } = item;
   return (
     <div className='collection-item'>
       <div className='image' style={{backgroundImage: `url(${imageUrl})`}} />
@@ -11,9 +14,13 @@ const CollectionItem = ({id, name, price, imageUrl}) => {
           <span className='name'>{name}</span>
           <span className='price'>${price}</span>
         </div>
-        <CustomButton inverted>ADD TO CART</CustomButton>
+        <CustomButton onClick={() => addItem(item)} inverted>ADD TO CART</CustomButton>
     </div>
   )
 };
 
-export default CollectionItem
+const mapDispatchToProps =  dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,16 +1,22 @@
 import CartActionTypes from  './cart.types';
 
 const INITIAL_STATE = {
-  show: false
+  show: false,
+  cartItems: [],
 };
 
 const cartReducer = (state =INITIAL_STATE, action) => {
-  const { TOGGLE_CART_HIDDEN } = CartActionTypes;
+  const { TOGGLE_CART_HIDDEN, ADD_ITEM } = CartActionTypes;
   switch(action.type){
     case TOGGLE_CART_HIDDEN:
       return {
         ...state,
         show: !state.show
+      }
+    case ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload]
       }
     default:
       return state
