ddd1337fa0514a7784e0a161568a9c10f47c3bf4
changangus <63685366+changangus@users.noreply.github.com>
2021-01-30 19:29:21 -0500

    added redux persist for local storage

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<React.StrictMode>
+			<BrowserRouter>
+				<PersistGate persistor={persistor}>
+					<App />
+				</PersistGate>
+			</BrowserRouter>
+		</React.StrictMode>
+	</Provider>,
+	document.getElementById('root')
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,18 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['cart'],
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
